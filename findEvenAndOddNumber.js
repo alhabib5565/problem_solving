0f42633a59891c697f8f4e5a0e3b16c3ec071550
alhabib5565 <alhabib5565@gmail.com>
2023-12-25 12:23:41 +0600

    find even and odd number

---
+++ findEvenAndOddNumber.js
@@ -0,0 +1,17 @@
+const findEvenAndOddNumber = (array) => {
+  const evenOrOdd = {
+    even: [],
+    odd: [],
+  };
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (element % 2 !== 0) {
+      evenOrOdd.odd.push(element);
+    } else {
+      evenOrOdd.even.push(element);
+    }
+  }
+  console.log(evenOrOdd);
+};
+
+findEvenAndOddNumber([2, 3, 4, 5, 6, 7, 8, 9, 10]); // { even: [ 2, 4, 6, 8, 10 ], odd: [ 3, 5, 7, 9 ] }
